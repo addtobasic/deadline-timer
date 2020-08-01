aa4188fda955b59725787114454a28444fa33a34
Genshi0916 <genshi0916@gmail.com>
2020-08-01 21:48:24 +0900

    [追加]作業を進めた

---
+++ src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-footer
+      :absolute="!fixed"
+      app
+      color="#44bc84"
+      dark
+    >
+      <div class="flex-grow-1"></div>
+      <v-icon>mdi-gmail</v-icon>
+      genshi0916@gmail.com
+    </v-footer>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+  export default Vue.extend({
+    name: 'Footer',
+
+  })
+</script>
+<style>
+#gmail{
+  margin-right:3px;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+      <v-app-bar
+      app
+      color="#44bc84"
+      dark
+    >
+      <v-toolbar-title>When is the deadline ?</v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+  export default Vue.extend({
+    name: 'Header',
+
+  })
+</script>
